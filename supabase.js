1e59aaaf32be847e0444d887b69b746665017bf3
yinjunjing <3460473191@qq.com>
2026-04-19 23:15:31 +0800

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const supabaseUrl = "https://abeqawasmsqbrhepekz.supabase.co";
+const supabaseKey = "sb_publishable_zJwz3gsDDIhzEbocK2JcTQ_hsifE26W";
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+window.supabase = supabase;
